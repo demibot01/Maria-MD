be9342006a60e0de2957179cce6bd3249fecced8
demibot01 <therealdemipark2@gmail.com>
2024-07-05 02:11:10 +0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "639683547031"],
+global.ownername = process.env.OWNER_NAME || "demigoddess",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -40,14 +40,14 @@ global.autoread_status = false //auto view status/story
 //reply messages
 global.mess = {
     done: '🍭 *Here you go, darling!* ',
-    prem: '🍭 *My darling, this feature is reserved for premium users only*',
-    admin: '🍭 *My darling, this feature is reserved for admins only*',
-    botAdmin: '🍭 *Darling, this feature can only be used when the bot is a group admin* ',
-    owner: '🍭 *My dearest, this feature is reserved for the owner only*',
+    prem: '🍭 *My Lord, this feature is reserved for premium users only*',
+    admin: '🍭 *My Lord, this feature is reserved for admins only*',
+    botAdmin: '🍭 *Lord, this feature can only be used when the bot is a group admin* ',
+    owner: '🍭 *My Lord, this feature is reserved for the owner only*',
     group: '🍭 *Sweetheart, this feature is exclusively for groups*',
-    private: '🍭 *My love, this feature is exclusively for private chats*',
-    wait: '🍭 *Darling, in process...* ',    
-    error: '🍭 *Oh darling, there seems to be an error!*',
+    private: '🍭 *My Lord, this feature is exclusively for private chats*',
+    wait: '🍭 *Lord, in process...* ',    
+    error: '🍭 *Oh, Lord, there seems to be an error!*',
 }
 
 module.exports = {
